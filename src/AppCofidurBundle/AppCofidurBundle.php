2589027eb3e95012dfc83b84b3218227608e639c
flodavid <fl.david.53@gmail.com>
2016-10-19 18:55:40 +0200

    Début override controller avec Listener

--- src/AppCofidurBundle/AppCofidurBundle.php
+++ src/AppCofidurBundle/AppCofidurBundle.php
@@ -6,10 +6,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppCofidurBundle extends Bundle
 {
-/*  Override the getParent method
+/*  Override the getParent method */
     public function getParent()
     {
         return 'FOSUserBundle';
     }
-*/
+
 }
---
+++ src/AppCofidurBundle/EventListener/PasswordResettingListener.php
@@ -0,0 +1,38 @@
+<?php
+namespace AppCofidurBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+* Listener responsible to change the redirection at the end of the password resetting
+*/
+class PasswordResettingListener implements EventSubscriberInterface
+{
+private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onPasswordResettingSuccess',
+        );
+    }
+
+    public function onPasswordResettingSuccess(FormEvent $event)
+    {
+        $url = $this->router->generate('homepage');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
