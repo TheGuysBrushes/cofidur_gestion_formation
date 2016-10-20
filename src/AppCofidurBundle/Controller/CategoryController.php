867e9dcca9c49fbd1b8ae83598b50fd60cfe8e4b
flodavid <fl.david.53@gmail.com>
2016-10-20 19:12:08 +0200

    Traduction de Tache en Task et Categorie en Category
    + Ajout de l'accent sur Catégorie dans les pages web.
    + Correction ou notation erreurs de phpstorm

--- src/AppCofidurBundle/Controller/CategoryController.php
+++ src/AppCofidurBundle/Controller/CategoryController.php
@@ -1,35 +1,35 @@
 <?php
 namespace AppCofidurBundle\Controller;
 
-use AppCofidurBundle\Entity\Categorie;
-use AppCofidurBundle\Form\Type\CategorieType;
+use AppCofidurBundle\Entity\Category;
+use AppCofidurBundle\Form\Type\CategoryType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class CategorieController extends Controller
+class CategoryController extends Controller
 {
 
 
     public function addAction(Request $request, $idForm)
     {
-        $categorie = new Categorie();
-        $categorie->setIdFormation($idForm);
+        $category = new Category();
+        $category->setIdFormation($idForm);
 
-        $form = $this->createForm(CategorieType::class, $categorie);
+        $form = $this->createForm(CategoryType::class, $category);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $categorie = $form->getData();
+            $category = $form->getData();
 
             $em = $this->getDoctrine()->getManager();
-            $em->persist($categorie);
+            $em->persist($category);
             $em->flush();
 
             return $this->redirectToRoute('AppCofidurBundle_formation_show', array('id' => $idForm));
         }
 
-        return $this->render('AppCofidurBundle:Page/Categorie:categorie_add.html.twig', array(
+        return $this->render('AppCofidurBundle:Page/Category:category_add.html.twig', array(
             'form' => $form->createView(),
         ));    
     }   
@@ -39,22 +39,22 @@ class CategorieController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $categorie = $em->getRepository('AppCofidurBundle:Categorie')->find($idCat);
+        $category = $em->getRepository('AppCofidurBundle:Category')->find($idCat);
 
-        $form = $this->createForm(CategorieType::class, $categorie);
+        $form = $this->createForm(CategoryType::class, $category);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $categorie = $form->getData();
+            $category = $form->getData();
 
             $em = $this->getDoctrine()->getManager();
-            $em->persist($categorie);
+            $em->persist($category);
             $em->flush();
 
             $requete_formation = $em->createQuery('
-                SELECT c.idFormation FROM AppCofidurBundle:Categorie c WHERE c.id = :idCategorie')
-            ->setParameter('idCategorie', $idCat);
+                SELECT c.idFormation FROM AppCofidurBundle:Category c WHERE c.id = :idCategory')
+                ->setParameter('idCategory', $idCat);
 
             $formation = $requete_formation->getResult();
 
@@ -64,7 +64,7 @@ class CategorieController extends Controller
             return $this->redirectToRoute('AppCofidurBundle_formation_show', array('id' => $formation_id));
         }
 
-        return $this->render('AppCofidurBundle:Page/Categorie:categorie_edit.html.twig', array(
+        return $this->render('AppCofidurBundle:Page/Category:category_edit.html.twig', array(
             'form' => $form->createView(),
         ));    
     }   
@@ -74,26 +74,26 @@ class CategorieController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $categorie = $em->getRepository('AppCofidurBundle:Categorie')->find($idCat);
-        $taches = $em->getRepository('AppCofidurBundle:Tache')->findBy(array('idCategorie'=>$idCat));
+        $category = $em->getRepository('AppCofidurBundle:Category')->find($idCat);
+        $tasks = $em->getRepository('AppCofidurBundle:Task')->findBy(array('idCategory' => $idCat));
 
-        if (!$categorie) {
+        if (!$category) {
             throw $this->createNotFoundException('Pas d\'objet');
         }
 
         $requete_formation = $em->createQuery('
-            SELECT c.idFormation FROM AppCofidurBundle:Categorie c WHERE c.id = :idCategorie')
-        ->setParameter('idCategorie', $idCat);
+            SELECT c.idFormation FROM AppCofidurBundle:Category c WHERE c.id = :idCategory')
+            ->setParameter('idCategory', $idCat);
 
         $formation = $requete_formation->getResult();
 
         $formation_id = $formation[0]['idFormation'];
 
 
-        $em->remove($categorie);
+        $em->remove($category);
 
-        foreach ($taches as $tache) 
-            $em->remove($tache);
+        foreach ($tasks as $task)
+            $em->remove($task);
 
         $em->flush();
 
