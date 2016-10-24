9c3517f67baf45fc4acdc5f9d351c6cb75c19e76
vdeleeuw <valerian.deleeuw@gmail.com>
2016-10-24 10:29:12 +0200

    operatorcategory addeditdelete

---
+++ src/AppCofidurBundle/Controller/OperatorTaskController.php
@@ -0,0 +1,10 @@
+<?php
+namespace AppCofidurBundle\Controller;
+
+
+use AppCofidurBundle\Entity\OperatorTask;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class OperatorTaskController extends Controller
+{
+}
