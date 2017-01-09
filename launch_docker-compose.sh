445369f93c36fe4701b99c02880128c868de2a98
flodavid <fl.david.53@gmail.com>
2017-01-09 18:09:23 +0100

    création de scripts de lancement docker

---
+++ launch_docker-compose.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Composition des conteneurs docker."
+echo "Pour stopper l'exécution, exécutez la comm&ande 'fg', puis Ctrl+C"
+docker-compose up -d &
---
+++ launch_server_double-bash.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Ajout des droits aux dossier logs et cache"
+sudo chmod -R 666 symfony/app/cache symfony/app/logs
+
+echo "Composition des conteneurs docker."
+echo "Pour stopper l'exécution, exécutez la comm&ande 'fg', puis Ctrl+C"
+bash -x ./launch_docker-compose.sh
+#docker-compose up -d &
+
+grep_result=$(docker inspect $(docker ps -f name=nginx -q) | grep '                    \"IPAddress\":')
+
+grep_result=$(echo $grep_result | cut -d':' -f 2)
+IPAddress=${grep_result#*\"}   # remove prefix ending in """
+IPAddress=${IPAddress%\"*}   # remove suffix starting with """
+
+echo "Adresse d'accès locale : http://"$IPAddress
+
+docker-compose exec php bash
+
